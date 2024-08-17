0cda4fe348b5ffab6f4474d6934fa09e21202fb3
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-08-17 12:42:32 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 // Import Express
 const express = require("express");
 
-
 // Import CORS
 const cors = require("cors");
-
 
 // create an express application
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // define a port
 const PORT = 4000;
+
+
+
+
+
+
+// create a simple route
+app.get("/items", (request, response, next) => {
+    response.json (snacks);
+  });
+  
+    // GET /items/:id: Retrieves a specific snack by its ID
+  app.get("/items/:id", (request, respond, next) => {
+    const snack = snacks.find(s => s.id === parseInt(request.params.id));
+    if (!snack) return next(); // 404 if not found
+    response.json(snack);
+  });
+    
+  // POST /items: Adds a new snack to the list
+  app.post("/items", (request, response) => {
+    const newSnack = {
+      id: snacks.length + 1,
+      ...request.body
+    };
+    snacks.push(newSnack);
+    response.status(201).json(newSnack);
+  });
+
+  
